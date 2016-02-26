1104f1b13823c49fdbb4cac823899f12aa597614
Aksenchyk <aksenchyk.v@gmail.com>
2016-02-26 11:17:34 -0800

    chore: more respond info

--- src/web/client/app/app.ts
+++ src/web/client/app/app.ts
@@ -18,7 +18,15 @@ export class App {
         this._http.post("/api/test", null)
             .map(res => res.json())
             .subscribe((result) => {
-               this.message = `Server respond: ${result} is the result of coordinated work of 5 microservices and means, that the application works as designed`;
+                console.log(result);
+               this.message = `
+                    Server respond: "${result.username}" is the result of coordinated work of 5 microservices. 
+                    That means, that the application works as designed. 
+                    See logs for more info
+               `;
+            },(err)=>{
+                 console.log(err);
+                 this.message = "ERROR"
             })
     }
 }
--- src/web/server/controllers/api.controller.js
+++ src/web/server/controllers/api.controller.js
@@ -1,8 +1,9 @@
 var seneca = require("seneca")();
 
 var userService = seneca.client({
-    host: process.env.USERS_HOST,
-    port: process.env.USERS_PORT,
+    // NOTE! this variables are set only when service starts within a container
+    host: process.env.USERS_HOST || "localhost", 
+    port: +process.env.USERS_PORT || 3031, // 3031 - port of users service, running within a container
     pin: {
         role: 'user',
         cmd: '*'
@@ -13,7 +14,7 @@ module.exports = {
     testRoute: function (req, res, next) {
         userService.act({ role: 'user', cmd: 'create', username: "username" }, (err, user) => {
             console.log("userservice responded: ", user);
-            if(err) return res.status(500).send();
+            if (err) return res.status(500).send();
             return res.status(200).send(user);
         });
     }
