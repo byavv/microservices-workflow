90ad1f1c2d4150498bf45da3955195d508c2d9fd
byavv <aksslavvv@gmail.com>
2016-02-27 11:07:25 +0300

    refactor: regular refactoring

--- src/web/server/controllers/api.controller.js
+++ src/web/server/controllers/api.controller.js
@@ -2,8 +2,8 @@ var seneca = require("seneca")();
 
 var userService = seneca.client({
     // NOTE! this variables are set only when service starts within a container
-    host: process.env.USERS_HOST || "localhost", 
-    port: +process.env.USERS_PORT || 3031, // 3031 - port of users service, running within a container
+    host: process.env.USERS_HOST || "localhost",
+    port: +process.env.USERS_PORT || 3031, // 3031 - exported port of users service, running within a container 
     pin: {
         role: 'user',
         cmd: '*'
@@ -11,7 +11,7 @@ var userService = seneca.client({
 });
 
 module.exports = {
-    testRoute: function (req, res, next) {
+    testRoute: (req, res) => {
         userService.act({ role: 'user', cmd: 'create', username: "username" }, (err, user) => {
             console.log("userservice responded: ", user);
             if (err) return res.status(500).send();
--- src/web/server/routes/api.routes.js
+++ src/web/server/routes/api.routes.js
@@ -1,7 +1,5 @@
 'use strict';
-
 var api = require('../controllers/api.controller');  
-
 
 module.exports = function (app) {
 	app.route("/api/test").post(api.testRoute);	
